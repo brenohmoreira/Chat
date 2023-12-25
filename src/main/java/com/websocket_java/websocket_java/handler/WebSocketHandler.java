8c4ec531547c6cdabcb6d18cd88c78cdb71da2e5
Breno <breno.am2004@gmail.com>
2023-12-24 23:22:22 -0300

    enviando e mensagem corretamente

--- src/main/java/com/websocket_java/websocket_java/handler/WebSocketHandler.java
+++ src/main/java/com/websocket_java/websocket_java/handler/WebSocketHandler.java
@@ -10,39 +10,38 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
     // Executado assim que a conexão é estabelecida
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("[afterConnectionEstablished] session id " + session.getId());
-
-        // É possível enviar mensagens do back-end
-        session.sendMessage(new TextMessage("Mensagem do back-end!"));
-
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                try {
-                    session.sendMessage(new TextMessage("Olá " + UUID.randomUUID()));
-                }
-                catch(IOException error) {
-                    System.out.println(error.getMessage());
-                }
-            }
-        }, 2000L, 2000L);
+        sessions.add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("[handleTextMessage] message " + message.getPayload());
+        /*
+            Este método é chamado quando uma mensagem de texto é recebida de um cliente.
+            Percorre todas as sessões WebSocket na lista e envia a mensagem para cada uma delas.
+
+            Cada sessão tem um id que representa a pessoa. O for abaixo percorre todas as sessões e envia a mensagem para todos.
+            Se você desejar, por exemplo, que a mensagem não seja enviada para o usuário que mandou a mensagem, então basta usar a session
+            passada como parâmetro neste método. Acontece que, diferente de sessions - que possui todas as sessões -, a session se trata
+            da sessão do usuário que está conectado. Assim sendo, utilizando getId(), fica simples não enviar a mensagem para a pessoa x ou y.
+         */
+        for(WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(message);
+        }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("[afterConnectionClosed] session id " + session.getId());
+        sessions.remove(session);
     }
 
 
